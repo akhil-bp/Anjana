f0be34115247845c14023e856665c5776c59927a
Akhil B P <akhilbp94@gmail.com>
2025-05-02 10:26:22 +0530

    add login

---
+++ login.tsx
@@ -0,0 +1,12 @@
+// @flow 
+import * as React from 'react';
+type Props = {
+    
+};
+export const login = (props: Props) => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
